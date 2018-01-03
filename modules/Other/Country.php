5764b94cb2fb08ac45107dcb91f84c61796388b1
myidcn <yufeng869@163.com>
2018-01-03 09:17:02 +0800

    yetiforce 4.3.0中文语言包

---
+++ modules/Other/Country.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Country 简体中文语言包
+ * @package www.idercn.com
+ * @版权所有 www.idercn.com
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @作者 www.idercn.com <idmaster@163.com>
+ */
+$languageStrings = [
+	'Antigua And Barbuda' => '安提瓜和巴布达',
+	'Bosnia And Herzegovina' => '波斯尼亚和黑塞哥维那',
+	'Brunei Darussalam' => '文莱',
+	'Cocos (keeling) Islands' => '科科斯群岛',
+	'Congo' => '刚果-布拉柴维尔',
+	'Congo, The Democratic Republic Of The' => '刚果-金沙萨',
+	'Cote D\'ivoire' => '科特迪瓦',
+	'Falkland Islands (malvinas)' => '福克兰群岛',
+	'Guinea-bissau' => '几内亚比绍',
+	'Holy See (vatican City State)' => '梵蒂冈',
+	'Hong Kong' => '中国香港特别行政区',
+	'Iran, Islamic Republic Of' => '伊朗',
+	'Kazakstan' => '哈萨克斯坦',
+	'Korea, Democratic People\'s Republic Of' => '朝鲜',
+	'Korea, Republic Of' => '韩国',
+	'Lao People\'s Democratic Republic' => '老挝',
+	'Libyan Arab Jamahiriya' => '利比亚',
+	'Macau' => '中国澳门特别行政区',
+	'Macedonia, The Former Yugoslav Republic Of' => '马其顿',
+	'Micronesia, Federated States Of' => '密克罗尼西亚',
+	'Moldova, Republic Of' => '摩尔多瓦',
+	'Myanmar' => '缅甸 (缅甸)',
+	'Palestinian Territory, Occupied' => '巴勒斯坦领土',
+	'Pitcairn' => '皮特凯恩群岛',
+	'Reunion' => '法属非洲留尼旺岛',
+	'Russian Federation' => '俄罗斯',
+	'Saint Helena' => '圣海伦娜',
+	'Saint Kitts And Nevis' => '圣圣基茨和尼维斯',
+	'Saint Lucia' => '圣卢西亚',
+	'Saint Pierre And Miquelon' => '圣皮埃尔克隆',
+	'Saint Vincent And The Grenadines' => '圣文森特和格林纳丁斯',
+	'Sao Tome And Principe' => '圣多美和普林西比',
+	'South Georgia And The South Sandwich Islands' => '南乔治亚岛南三明治群岛',
+	'Svalbard And Jan Mayen' => '斯瓦尔巴德岛和扬马延岛 ',
+	'Syrian Arab Republic' => '叙利亚',
+	'Taiwan, Province Of China' => '中国台湾',
+	'Tanzania, United Republic Of' => '坦桑尼亚',
+	'Trinidad And Tobago' => '特立尼达和多巴哥',
+	'Turks And Caicos Islands' => '特克斯和凯科斯群岛',
+	'United States Minor Outlying Islands' => '美国离岛',
+	'Viet Nam' => '越南',
+	'Virgin Islands, British' => '英属维尔京群岛',
+	'Virgin Islands, U.s.' => '美属维尔京群岛',
+	'Wallis And Futuna' => '瓦利斯群岛',
+];
